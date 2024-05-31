b201b9e2c2b59e70659a6b01f723b4069099d131
grafica <paolo@monema.it>
2024-05-31 17:32:41 +0200

    inizio stile mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import style from "./home.module.scss";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { MdOutlineChevronRight } from "react-icons/md";
 import Form from "./components/form/form";
 import { useState } from "react";
@@ -74,225 +74,242 @@ export default function Home() {
   setTimeout(() => {
     setSplash(false);
   }, 1000);
-  if (splash) {
-    return <Splash />;
-  } else {
-    return (
-      <main className={style.home}>
-        <section className={style.FirstSectionHome}>
-          <motion.div
-            className={style.FirstSectionHome__text}
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
-          >
-            <h1>
-              <Image
-                src={"/image/bullet.svg"}
-                width={40}
-                height={40}
-                alt="bullet image"
-              />
-              Da oltre 20 anni, un servizio di prim’ordine
-            </h1>
-            <div className={style.FirstSectionHome__text__content}>
-              <p>
-                Gestiamo le richieste con competenza, assicurando che la tua
-                esperienza sia fluida e senza problemi. Siamo partner
-                affidabili. Collaboriamo attivamente con i nostri clienti per
-                capire le loro esigenze specifiche e offrire soluzioni
-                personalizzate. La nostra flessibilità e volontà di adattarci
-                alle richieste dei clienti sono ciò che ci rende un’eccellenza
-                nel nostro settore.
-                <Image
-                  src={"/image/iso.jpg"}
-                  width={300}
-                  height={145}
-                  alt="iso image"
-                />
-              </p>
-              <ul>
-                <li>1 sede Italia</li>
-                <li>1 Subsidiary Svizzera</li>
-                <li>1 Subsidiary USA</li>
-              </ul>
-            </div>
-          </motion.div>
-          <div className={style.FirstSectionHome__image}>
-            <Image
-              src={"/image/vts.jpg"}
-              width={1148}
-              height={519}
-              alt="first section image"
-            />
-          </div>
-        </section>
+
+  return (
+    <AnimatePresence>
+      {splash ? (
         <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          key="splash"
+          initial={{ backgroundColor: "#283443", opacity: 1 }}
+          animate={{ backgroundColor: "#485f7d", opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
         >
-          <Image
-            className={style.dividerHome}
-            src={"/image/dividerHome.jpg"}
-            width={300}
-            height={300}
-            alt="divider"
-            unoptimized
-          />
+          <Splash />
         </motion.div>
-        <motion.section
-          className={style.secondSectionHome}
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
-        >
-          <h2>
-            <Image
-              src={"/image/bullet.svg"}
-              width={40}
-              height={40}
-              alt="bullet image"
-            />
-            Offriamo consulenza e servizi di Commissioning, Qualification and
-            Validation
-          </h2>
-          <div className={style.secondSectionHome__mainText}>
-            <p>
-              Consulenza specializzata per le attività di Commissioning,
-              Qualifica e Validazioni di macchinari, apparecchiature, sistemi di
-              controllo e automazione, sistemi informatici per la gestione di
-              fabbrica, utilities, ambienti a contaminazione controllata per
-              produzioni sterili.
-            </p>
-            <p>
-              Attività di monitoraggio dei locali a contaminazione controllata,
-              taratura della strumentazione critica, consulenza sull’analisi dei
-              rischi e procedure operative standard dell’industria farmaceutica,
-              in accordo a quanto richiesto dalle normative che regolamentano le
-              attività negli opifici di produzione. Noleggio di strumenti per la
-              qualifica e taratura.
-            </p>
-          </div>
-          <div className={style.secondSectionHome__specs}>
-            {secondSectionHomeData.specs.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className={style.secondSectionHome__specs__singleSpec}
-                >
-                  <motion.div
-                    initial={{ opacity: 0, y: 100 }}
-                    whileInView={{
-                      opacity: 1,
-                      y: 0,
-                      transition: { duration: 1 },
-                    }}
-                  >
-                    <Image
-                      src={`/image/${item.image}`}
-                      width={90}
-                      height={120}
-                      alt="iso image"
-                    />
-                  </motion.div>
-                  <div
-                    className={style.secondSectionHome__specs__singleSpec__txt}
-                  >
-                    <h3>{item.title}</h3>
-                    <ul>
-                      {item.list.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </motion.section>
-        <motion.section
-          className={style.thirdSectionHome}
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
-        >
-          <div className={style.thirdSectionHome__wrapper}>
+      ) : (
+        <main className={style.home}>
+          <section className={style.FirstSectionHome}>
             <motion.div
-              className={style.thirdSectionHome__wrapper__text}
+              className={style.FirstSectionHome__text}
               initial={{ opacity: 0, y: 100 }}
               whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
             >
-              <h2>
-                {" "}
+              <h1>
                 <Image
                   src={"/image/bullet.svg"}
                   width={40}
                   height={40}
                   alt="bullet image"
                 />
-                Lavora con noi
-              </h2>
-              <p>
-                La nostra azienda è sempre alla ricerca di personale
-                qualificato.
-                <br />
-                Se sei un professionista ambizioso e desideri unirti a un team
-                dinamico e orientato al successo, candidati per una delle
-                posizioni aperte{" "}
-              </p>
-              <button>
-                <span>Vai alle candidature</span>
-                <MdOutlineChevronRight />
-              </button>
-            </motion.div>
-            <motion.div
-              className={style.thirdSectionHome__wrapper__image}
-              initial={{ opacity: 0, x: 100 }}
-              whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
-            >
-              <Image
-                src={"/image/lavora.png"}
-                width={467}
-                height={479}
-                alt="iso image"
-              />
-            </motion.div>
-            <div className={style.thirdSectionHome__wrapper__image__back}></div>
-          </div>
-          <div className={style.thirdSectionHome__background}></div>
-        </motion.section>
-        <section className={style.fourthSectionHome}>
-          <div className={style.fourthSectionHome__wrapper}>
-            <Image
-              src={"/image/contatti.jpg"}
-              layout="fill"
-              alt={"iso image"}
-            />
-            <div className={style.fourthSectionHome__wrapper__text}>
-              <div className={style.fourthSectionHome__wrapper__text__content}>
-                <div
-                  className={
-                    style.fourthSectionHome__wrapper__text__content__box
-                  }
-                >
-                  <h2>
-                    {" "}
-                    <Image
-                      src={"/image/bullet.svg"}
-                      width={40}
-                      height={40}
-                      alt="bullet image"
-                    />
-                    Contattaci
-                  </h2>
-                  <p>
-                    Se desideri ricevere ulteriori informazioni sui nostri
-                    servizi, se hai domande o richieste, compila il form
-                  </p>
-                </div>
+                Da oltre 20 anni, un servizio di prim’ordine
+              </h1>
+              <div className={style.FirstSectionHome__text__content}>
+                <p>
+                  Gestiamo le richieste con competenza, assicurando che la tua
+                  esperienza sia fluida e senza problemi. Siamo partner
+                  affidabili. Collaboriamo attivamente con i nostri clienti per
+                  capire le loro esigenze specifiche e offrire soluzioni
+                  personalizzate. La nostra flessibilità e volontà di adattarci
+                  alle richieste dei clienti sono ciò che ci rende un’eccellenza
+                  nel nostro settore.
+                  <Image
+                    src={"/image/iso.jpg"}
+                    width={300}
+                    height={145}
+                    alt="iso image"
+                  />
+                </p>
+                <ul>
+                  <li>1 sede Italia</li>
+                  <li>1 Subsidiary Svizzera</li>
+                  <li>1 Subsidiary USA</li>
+                </ul>
               </div>
-              <Form />{" "}
+            </motion.div>
+            <div className={style.FirstSectionHome__image}>
+              <Image
+                src={"/image/vts.jpg"}
+                width={1148}
+                height={519}
+                alt="first section image"
+              />
             </div>
-          </div>
-        </section>
-      </main>
-    );
-  }
+          </section>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          >
+            <Image
+              className={style.dividerHome}
+              src={"/image/dividerHome.jpg"}
+              width={300}
+              height={300}
+              alt="divider"
+              unoptimized
+            />
+          </motion.div>
+          <motion.section
+            className={style.secondSectionHome}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          >
+            <h2>
+              <Image
+                src={"/image/bullet.svg"}
+                width={40}
+                height={40}
+                alt="bullet image"
+              />
+              Offriamo consulenza e servizi di Commissioning, Qualification and
+              Validation
+            </h2>
+            <div className={style.secondSectionHome__mainText}>
+              <p>
+                Consulenza specializzata per le attività di Commissioning,
+                Qualifica e Validazioni di macchinari, apparecchiature, sistemi
+                di controllo e automazione, sistemi informatici per la gestione
+                di fabbrica, utilities, ambienti a contaminazione controllata
+                per produzioni sterili.
+              </p>
+              <p>
+                Attività di monitoraggio dei locali a contaminazione
+                controllata, taratura della strumentazione critica, consulenza
+                sull’analisi dei rischi e procedure operative standard
+                dell’industria farmaceutica, in accordo a quanto richiesto dalle
+                normative che regolamentano le attività negli opifici di
+                produzione. Noleggio di strumenti per la qualifica e taratura.
+              </p>
+            </div>
+            <div className={style.secondSectionHome__specs}>
+              {secondSectionHomeData.specs.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={style.secondSectionHome__specs__singleSpec}
+                  >
+                    <motion.div
+                      initial={{ opacity: 0, y: 100 }}
+                      whileInView={{
+                        opacity: 1,
+                        y: 0,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                      <Image
+                        src={`/image/${item.image}`}
+                        width={90}
+                        height={120}
+                        alt="iso image"
+                      />
+                    </motion.div>
+                    <div
+                      className={
+                        style.secondSectionHome__specs__singleSpec__txt
+                      }
+                    >
+                      <h3>{item.title}</h3>
+                      <ul>
+                        {item.list.map((item, index) => (
+                          <li key={index}>{item}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </motion.section>
+          <motion.section
+            className={style.thirdSectionHome}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          >
+            <div className={style.thirdSectionHome__wrapper}>
+              <motion.div
+                className={style.thirdSectionHome__wrapper__text}
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+              >
+                <h2>
+                  {" "}
+                  <Image
+                    src={"/image/bullet.svg"}
+                    width={40}
+                    height={40}
+                    alt="bullet image"
+                  />
+                  Lavora con noi
+                </h2>
+                <p>
+                  La nostra azienda è sempre alla ricerca di personale
+                  qualificato.
+                  <br />
+                  Se sei un professionista ambizioso e desideri unirti a un team
+                  dinamico e orientato al successo, candidati per una delle
+                  posizioni aperte{" "}
+                </p>
+                <button>
+                  <span>Vai alle candidature</span>
+                  <MdOutlineChevronRight />
+                </button>
+              </motion.div>
+              <motion.div
+                className={style.thirdSectionHome__wrapper__image}
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+              >
+                <Image
+                  src={"/image/lavora.png"}
+                  width={467}
+                  height={479}
+                  alt="iso image"
+                />
+              </motion.div>
+              <div
+                className={style.thirdSectionHome__wrapper__image__back}
+              ></div>
+            </div>
+            <div className={style.thirdSectionHome__background}></div>
+          </motion.section>
+          <section className={style.fourthSectionHome}>
+            <div className={style.fourthSectionHome__wrapper}>
+              <Image
+                src={"/image/contatti.jpg"}
+                layout="fill"
+                alt={"iso image"}
+              />
+              <div className={style.fourthSectionHome__wrapper__text}>
+                <div
+                  className={style.fourthSectionHome__wrapper__text__content}
+                >
+                  <div
+                    className={
+                      style.fourthSectionHome__wrapper__text__content__box
+                    }
+                  >
+                    <h2>
+                      {" "}
+                      <Image
+                        src={"/image/bullet.svg"}
+                        width={40}
+                        height={40}
+                        alt="bullet image"
+                      />
+                      Contattaci
+                    </h2>
+                    <p>
+                      Se desideri ricevere ulteriori informazioni sui nostri
+                      servizi, se hai domande o richieste, compila il form
+                    </p>
+                  </div>
+                </div>
+                <Form />{" "}
+              </div>
+            </div>
+          </section>
+        </main>
+      )}
+    </AnimatePresence>
+  );
 }
