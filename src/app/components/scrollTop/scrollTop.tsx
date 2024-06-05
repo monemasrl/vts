20fe04122897bb9e8404729cdc8153b37a717cef
grafica <paolo@monema.it>
2024-06-05 15:39:24 +0200

    test form

--- src/app/components/scrollTop/scrollTop.tsx
+++ src/app/components/scrollTop/scrollTop.tsx
@@ -1,15 +1,27 @@
 "use client";
 import style from "./scrollTop.module.scss";
 import { RxArrowUp } from "react-icons/rx";
-import { useScroll } from "framer-motion";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
 
 function ScrollTop() {
   const { scrollY } = useScroll();
-  console.log(scrollY.get());
+  const [visible, setVisible] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 400) setVisible(true);
+    else setVisible(false);
+  });
+
   return (
-    <div className={style.scrollTop}>
+    <motion.div
+      className={style.scrollTop}
+      style={{ opacity: visible ? 1 : 0, transition: "opacity 0.5s" }}
+      transition={{ duration: 0.5 }}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
       <RxArrowUp />
-    </div>
+    </motion.div>
   );
 }
 
