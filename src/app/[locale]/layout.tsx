68e27bb9abea9cfb70d3ce9ff423ab08efd83b5f
grafica <paolo@monema.it>
2024-10-02 11:19:28 +0200

    test metadati

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,120 +13,6 @@ export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
 const oswald = Oswald({ subsets: ["latin"], weight: ["200", "400", "700"] });
-const mdata = {
-  it: {
-    metaHtml: {
-      title: "VTServices website",
-      description:
-        "Commissioning, qualification, validation. Da oltre 20 anni, un servizio di prim'ordine. La nostra flessibilità e volontà di adattarci alle richieste dei clienti sono ciò che ci rende un’eccellenza nel nostro settore.",
-      keywords:
-        "VTServices, Commissioning, qualification, validation, technical services",
-      openGraph: {
-        title: "VTServices website",
-        description:
-          "Commissioning, qualification, validation. Da oltre 20 anni, un servizio di prim'ordine. La nostra flessibilità e volontà di adattarci alle richieste dei clienti sono ciò che ci rende un’eccellenza nel nostro settore.",
-        type: "website",
-        locale: "it",
-        url: "https://vtservices.it/",
-        siteName: "VTServices",
-        images: [
-          {
-            url: "/image/opengraph-image.jpg",
-            width: 1200,
-            height: 630,
-            alt: "VTServices website",
-          },
-        ],
-      },
-    },
-    jsonld: {
-      "@context": "https://schema.org",
-      "@type": "Organization",
-      image: [
-        "https://vtservices.it/image/4_3graph.jpg",
-        "https://vtservices.it/image/mainimage.jpg",
-      ],
-      name: "VTServices",
-      address: [
-        {
-          "@type": "PostalAddress",
-          streetAddress: " via della Stazione 27 Mologno Barga",
-          addressLocality: "Lucca",
-          addressRegion: "Toscana",
-          postalCode: "55051",
-          addressCountry: "IT",
-        },
-        {
-          "@type": "PostalAddress",
-          streetAddress: "8000 Avalon Blvd, Suite 100 and 200",
-          addressLocality: "Alpharetta",
-          addressRegion: "Atlanta",
-          postalCode: "30009",
-          addressCountry: "GA",
-        },
-      ],
-      url: "https://vtservices.it/",
-      telephone: "+320583711372",
-      email: "info@vtservices.it",
-    },
-  },
-  en: {
-    metaHtml: {
-      title: "VTServices website",
-      description:
-        "First-class service for over 20 years, our flexibility and willingness to adapt to customer requests are what make us an excellence in our sector.",
-      keywords:
-        "VTServices, Commissioning, qualification, validation, technical services",
-      openGraph: {
-        title: "VTServices website",
-        description:
-          "First-class service for over 20 years, our flexibility and willingness to adapt to customer requests are what make us an excellence in our sector.",
-        type: "website",
-        locale: "en",
-        url: "https://vtservices.it/",
-        siteName: "VTServices",
-        images: [
-          {
-            url: "/image/opengraph-image.jpg",
-            width: 1200,
-            height: 630,
-            alt: "VTServices website",
-          },
-        ],
-      },
-    },
-    jsonld: {
-      "@context": "https://schema.org",
-      "@type": "Organization",
-      image: [
-        "https://vtservices.it/image/4_3graph.jpg",
-        "https://vtservices.it/image/mainimage.jpg",
-      ],
-      name: "VTServices",
-      address: [
-        {
-          "@type": "PostalAddress",
-          streetAddress: " via della Stazione 27 Mologno Barga",
-          addressLocality: "Lucca",
-          addressRegion: "Toscana",
-          postalCode: "55051",
-          addressCountry: "IT",
-        },
-        {
-          "@type": "PostalAddress",
-          streetAddress: "8000 Avalon Blvd, Suite 100 and 200",
-          addressLocality: "Alpharetta",
-          addressRegion: "Atlanta",
-          postalCode: "30009",
-          addressCountry: "GA",
-        },
-      ],
-      url: "https://vtservices.it/",
-      telephone: "+320583711372",
-      email: "info@vtservices.it",
-    },
-  },
-};
 
 export async function generateMetadata({
   params: { locale },
@@ -135,13 +21,9 @@ export async function generateMetadata({
 }) {
   //const data = mdata[locale as keyof typeof meta].metaHtml;
   if (locale === "it") {
-    return {
-      title: "vtServices",
-    };
+    return meta.it.meta;
   } else if (locale === "en") {
-    return {
-      title: "vtServices",
-    };
+    return meta.en.meta;
   }
 }
 
